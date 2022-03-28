ff55c71aa6ffd9d3dfef58d2519bbab74a606654
Ryan-Elbanna <93952585+Ryan-Elbanna@users.noreply.github.com>
2022-03-28 14:24:11 -0700

    commit

--- story.js
+++ story.js
@@ -1,4 +1,19 @@
 // JavaScript will be written soon.
 // This is ONLY a demo.
 
-var 
+var mainStory = {
+
+    start: {
+        text:"Sample text",
+        button:[
+            ["choice1", "Yes choice"],
+            ["choice2", "No choice option"],
+            ["choice3", "Neutral Choice"]
+
+        ]
+    }
+        
+
+};
+
+var mainStoryStorage = ["start"];
