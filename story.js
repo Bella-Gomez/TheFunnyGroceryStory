a2d667796bafaeb866dd8d0cee493373e2b0ce38
Ryan-Elbanna <93952585+Ryan-Elbanna@users.noreply.github.com>
2022-04-06 12:38:19 -0700

    commit

--- story.js
+++ story.js
@@ -1,68 +1,90 @@
 // JavaScript will be written soon.
 // This is ONLY a demo.
-
-
-var textFielder = document.getElementById("text");
-var buttonFielder = document.getElementById("buttonContainer");
-var buttons = document.createElement("button");
-var mainStoryStorage = ["start"];
-
-var mainStory = {
-
-    start: {
-        text: "Sample text, Sample text, Sample text, Sample text, Sample text, Sample text, Sample text, Sample text, ",
-        choices: [
-            ["secondPart", "Yes choice"],
-            ["choice2", "No choice option"],
-            ["choice3", "Neutral Choice"]
-        ]
-    },
-
-    choice3: {
-        text: "You picked choice #3!",
-        choices: [
-            [],
-            [],
-            []
-        ]
-    },
-
-    secondPart: {
-        text: "Woah, the second part is here",
-        choices: [
-            ["otherPart", "1 choice"],
-            ["lol", "2 choice"],
-            ["lol2", "3 choice"]
-        ]
+window.onload = function() {
+    var textFielder = document.getElementById("text");
+    var buttonFielder = document.getElementById("buttonContainer");
+    var youLose = document.getElementById("theEnd")
+    var mainStoryStorage = ["start"];
+    
+    var mainStory = {
+    
+        start: {
+            text: "Welcome to the party. What do you do?",
+            choices: [["goParty", "Go to the party"], ["dontParty", "Don't go"], ["yesTime", "Yes time"]]
+        },
+    
+        goParty: {
+            text: "You went to the party",
+            choices: [
+                ["Woo1", "Woah hey 1"],
+            ],
+        },
+    
+        dontParty: {
+            text: "You didn't go to the party",
+            choices: [
+                ["ye1", "hey 1"],
+            ],
+        },
+    
+        yesTime: {
+            text: "Just Yes",
+            choices: [
+                ["otherPart", "WOOOOOOO"],
+            ]
+        },
+    
+        otherPart: {
+            text: "Encountered End",
+            choices: [
+                [false, ""]
+            ]
+        }
+    };
+    
+    
+    function buttonMaker(textForButton, chosen) {
+        let buttons = document.createElement("button");
+        buttons.innerHTML = textForButton;
+        buttonFielder.appendChild(buttons);
+    
+        buttons.addEventListener("click", function() {mainStoryStorage.push(chosen); mainStoryMaker(); });
+    
+        if(textForButton == "") {
+            console.log("Encountered an Ending")
+            buttons.style.display = "none"
+        }
     }
-};
-
-
-function buttonMaker(textForButton, option) {
-    buttons.innerHTML = textForButton;
-    buttonFielder.appendChild(buttons);
-
-    buttons.addEventListener("click", function() {
-        mainStoryStorage.push(option); 
-        mainStoryMaker();})
-}
-
-
-function textMaker(textToAdd) {
-    textFielder.innerHTML = textToAdd;
-}
-
-function mainStoryMaker(text) { 
-    let storageDisplay = mainStoryStorage[mainStoryStorage.length - 1];
-    textFielder.innerHTML = "";
-    buttonFielder.innerHTML = "";
-
-    for(let sus of mainStoryStorage) {
-      textMaker(mainStory[sus].text)
-    }   
-    for (let sus of mainStory[storageDisplay].choices) {
-      buttonMaker(sus[1],sus[0]);
+    
+    
+    function textMaker(textToAdd) {
+        textFielder.innerHTML = textToAdd;
     }
-}
-
-mainStoryMaker(mainStory.start.text)
+    
+    function mainStoryMaker() { 
+        textFielder.innerHTML = "";
+        buttonFielder.innerHTML = "";
+    
+        for(let i of mainStoryStorage) {
+          textMaker(mainStory[i].text);
+          console.log(i);
+        }
+    
+        for (let choiceParts of mainStory[mainStoryStorage[mainStoryStorage.length - 1]].choices) {
+          buttonMaker(choiceParts[1], choiceParts[0]);
+          console.log(choiceParts);
+    
+          if(choiceParts[0] == false ) {
+            console.log("Cya");
+            youLose.style.display = ""
+            }
+        }
+    
+    }
+    mainStoryMaker();
+    
+    youLose.addEventListener("click", function(){location.reload(); });
+    youLose.style.display = "none"
+    
+    }
+    
